043a26df63eebbeccdfe36331c5b495f9437c7ac
Mohit <mr6114671@gmail.com>
2022-09-15 00:23:17 +0530

    pattern loops

---
+++ patterns/pattern1.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+    
+// int i,j,n=3;
+// for(i=1;i<=n;i++){
+//     for(j=1;j<=n;j++){
+//         cout<<i;
+//     }
+//     cout<<"\n";
+// }
+//     return 0;
+// }
+
+
+
+int main() {
+    
+int i,j,n=3;
+for(i=1;i<=n;i++){
+    for(j=1;j<=n;j++){
+        cout<<"*";
+    }
+    cout<<"\n";
+}
+    return 0;
+}
+  
